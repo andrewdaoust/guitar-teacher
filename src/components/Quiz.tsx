2749af33bc1c6c19dd3261adb2eaed1e0821867d
andrewdaoust <daoust.andrew@facilityconnex.com>
2023-11-06 08:18:38 -0500

    more quiz work

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 import { useState } from "react";
-import * as notes from '../lib/notes.ts';
-import "./Quiz.css"
+import * as notes from "../lib/notes.ts";
+import "./Quiz.css";
+
+import {
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+  Button,
+  ButtonGroup,
+  Avatar,
+  Stack,
+  Typography,
+  Box
+} from "@mui/material";
+// import FormControlLabel from "@mui/material/FormControlLabel";
+// import Checkbox from "@mui/material/Checkbox";
+// import Button from "@mui/material/Button"; 
+// import ButtonGroup from "@mui/material/ButtonGroup";
+// import Avatar from "@mui/material/Avatar"; 
 
 const MAX_COUNT = 20;
 const MAX_FRETS = 24;
@@ -16,10 +33,10 @@ export function Quiz() {
   const wrongAnswer = () => {
     setProgress({
       count: progress.count + 1,
-      score: progress.score
+      score: progress.score,
     });
     setQuestion(createQuestionData());
-  }
+  };
 
   const correctAnswer = () => {
     setProgress({
@@ -27,7 +44,7 @@ export function Quiz() {
       score: progress.score + 1,
     });
     setQuestion(createQuestionData());
-  }
+  };
 
   const resetGame = () => {
     setProgress({
@@ -35,66 +52,153 @@ export function Quiz() {
       score: 0,
     });
     setQuestion(createQuestionData());
-  }
+  };
 
-  return progress.count > 20 ?
-    <GameOver score={progress.score} resetFn={resetGame}/> :
-    <Question
-      progress={progress}
-      question={question}
-      onCorrect={correctAnswer}
-      onWrong={wrongAnswer}
-    />
-}
-
-
-// Supporting components
-function Question({progress, question, onCorrect, onWrong}) {
-  return (
-    <div className="quiz-game">
-      <p>
-        Question {progress.count}/{MAX_COUNT}
-      </p>
-      <p>Score: {progress.score}</p>
-      <p>String: {question.string}</p>
-      <p>Fret: {question.fret}</p>
-      <button
-        className="note-button"
-        onClick={question.options[0] === question.note ? onCorrect : onWrong}
-      >
-        {question.options[0]}
-      </button>
-      <button
-        className="note-button"
-        onClick={question.options[1] === question.note ? onCorrect : onWrong}
-      >
-        {question.options[1]}
-      </button>
-      <button
-        className="note-button"
-        onClick={question.options[2] === question.note ? onCorrect : onWrong}
-      >
-        {question.options[2]}
-      </button>
-      <button
-        className="note-button"
-        onClick={question.options[3] === question.note ? onCorrect : onWrong}
-      >
-        {question.options[3]}
-      </button>
-      <p>Answer: {question.note}</p>
+  return progress.count > 20 ? (
+    <QuizOver score={progress.score} resetFn={resetGame} />
+  ) : (
+    <div>
+      <QuizStart />
+      <Question
+        progress={progress}
+        question={question}
+        onCorrect={correctAnswer}
+        onWrong={wrongAnswer}
+      />
     </div>
   );
 }
 
-
-function GameOver({score, resetFn}) {
-  return (<div>
-    <p>Game over! Score: {score}/{MAX_COUNT}</p>
-    <button onClick={resetFn}>New Game</button>
-  </div>);
+// Supporting components
+function Question({ progress, question, onCorrect, onWrong }) {
+  return (
+    // <div className="quiz-game">
+    <Box justifyContent="center">
+      <Typography variant="h4">
+        Question {progress.count}/{MAX_COUNT}
+      </Typography>
+      <Typography variant="h4">SCORE: {progress.score}</Typography>
+      <Stack direction="row" spacing={1}>
+        <Typography variant="h5">STRING</Typography>
+        <Typography variant="h5">|</Typography>
+        <Typography variant="h5">FRET</Typography>
+      </Stack>
+      <Stack direction="row" spacing={2}>
+        <Avatar>{question.string}</Avatar>
+        <Avatar>{question.fret}</Avatar>
+      </Stack>
+      {/* <p>
+        String: <Avatar>{question.string}</Avatar>
+      </p> */}
+      {/* <p>Fret: {question.fret}</p> */}
+      {/* <button
+        className="note-button"
+        onClick={question.options[0] === question.note ? onCorrect : onWrong}
+      >
+        {question.options[0]}
+      </button> */}
+      <ButtonGroup size="large">
+        <QuizButton
+          option={question.options[0]}
+          note={question.note}
+          onCorrect={onCorrect}
+          onWrong={onWrong}
+        />
+        <QuizButton
+          option={question.options[1]}
+          note={question.note}
+          onCorrect={onCorrect}
+          onWrong={onWrong}
+        />
+        <QuizButton
+          option={question.options[2]}
+          note={question.note}
+          onCorrect={onCorrect}
+          onWrong={onWrong}
+        />
+        <QuizButton
+          option={question.options[3]}
+          note={question.note}
+          onCorrect={onCorrect}
+          onWrong={onWrong}
+        />
+      </ButtonGroup>
+      {/* <p>Answer: {question.note}</p> */}
+    </Box>
+    // </div>
+  );
 }
 
+function QuizButton({ option, note, onCorrect, onWrong }) {
+  return (
+    <Button
+      // className="note-button"
+      onClick={option === note ? onCorrect : onWrong}
+    >
+      {option}
+    </Button>
+  );
+}
+
+function QuizStart() {
+  const [stringState, setStringState] = React.useState({
+    "1": true,
+    "2": true,
+    "3": true,
+    "4": true,
+    "5": true,
+    "6": true,
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setStringState({
+      ...stringState,
+      [event.target.name]: event.target.checked,
+    });
+  };
+
+  return (
+    <div>
+      <FormGroup>
+        <FormControlLabel
+          control={<Checkbox defaultChecked onChange={handleChange} />}
+          label="6"
+        />
+        <FormControlLabel
+          control={<Checkbox defaultChecked onChange={handleChange} />}
+          label="5"
+        />
+        <FormControlLabel
+          control={<Checkbox defaultChecked onChange={handleChange} />}
+          label="4"
+        />
+        <FormControlLabel
+          control={<Checkbox defaultChecked onChange={handleChange} />}
+          label="3"
+        />
+        <FormControlLabel
+          control={<Checkbox defaultChecked onChange={handleChange} />}
+          label="2"
+        />
+        <FormControlLabel
+          control={<Checkbox defaultChecked onChange={handleChange} />}
+          label="1"
+        />
+      </FormGroup>
+    </div>
+  );
+}
+
+function QuizOver({ score, resetFn }) {
+  return (
+    <div>
+      <p>
+        Game over! Score: {score}/{MAX_COUNT}
+      </p>
+      <button onClick={resetFn}>New Game</button>
+    </div>
+  );
+}
 
 // Helper functions
 function createAnswers(correctNote: string): string[] {
